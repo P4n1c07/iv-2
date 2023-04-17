f99a7a8f402d5be1701828d51029cb146516f4ab
Ispa <pietropaolo.castagna@gmail.com>
2023-04-17 15:21:24 +0200

    with error in test

--- src/test/java/org/example/RectangleTest.java
+++ src/test/java/org/example/RectangleTest.java
@@ -9,7 +9,7 @@ public class RectangleTest {
     public void testRectangle(){
         Rectangle rec = new Rectangle(5,10);
 
-        assertEquals(5,rec.getLength());
+        assertEquals(3,rec.getLength());
         assertEquals(10,rec.getWidth());
         assertEquals(50,rec.getArea());
         assertEquals(30,rec.getPerimeter());
